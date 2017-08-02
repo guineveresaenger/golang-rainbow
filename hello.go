8e27f31c1df0599f45017667d815aaa4e542e613
Guinevere Saenger <guin@Guineveres-MacBook-Pro.local>
2017-08-02 16:01:29 -0700

    Second commit.

--- hello.go
+++ hello.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Hello, Guinevere. You're a real dev now. No pressure...\n")
+	fmt.Printf("Hello, Guinevere. You're a real dev now. No pressure...\n")
+	fmt.Printf("Hello, Guinevere. You're a real dev now. No pressure...\n")
+
+
 }

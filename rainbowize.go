eca37b4eca33258151c80c06304ee8368a93db9a
guineveresaenger <guineveresaenger@gmail.com>
2017-11-28 09:52:39 -0800

    exporting function

--- rainbowize.go
+++ rainbowize.go
@@ -7,7 +7,9 @@ import (
 	"github.com/fatih/color"
 )
 
-func rainbow(s string, lineCount int) {
+// Rainbow function will take a string and color it like a rainbow.
+// With increased lineCount, each subsequent string will be offset, so as to make diagonal color lines.
+func Rainbow(s string, lineCount int) {
 	for i := 0; i < len(s); i++ {
 
 		subindex := (i + lineCount) % 30 // will give the index of each 30 char substring and rotate through colors
@@ -20,11 +22,11 @@ func rainbow(s string, lineCount int) {
 		case subindex >= 10 && subindex < 15:
 			green(string(s[i]))
 		case subindex >= 15 && subindex < 20:
-			blue(string(s[i]))
-		case subindex >= 20 && subindex < 25:
-			magenta(string(s[i]))
-		case subindex >= 25 && subindex < 30:
 			cyan(string(s[i]))
+		case subindex >= 20 && subindex < 25:
+			blue(string(s[i]))
+		case subindex >= 25 && subindex < 30:
+			magenta(string(s[i]))
 		default:
 			fmt.Printf(string(s[i]))
 		}
@@ -44,14 +46,14 @@ func blue(s string) {
 	fmt.Printf("%s", strings.TrimSuffix(color.BlueString(s), " "))
 }
 
+func cyan(s string) {
+	fmt.Printf("%s", strings.TrimSuffix(color.CyanString(s), " "))
+}
+
 func green(s string) {
 	fmt.Printf("%s", strings.TrimSuffix(color.GreenString(s), " "))
 }
 
 func magenta(s string) {
 	fmt.Printf("%s", strings.TrimSuffix(color.MagentaString(s), " "))
-}
-
-func cyan(s string) {
-	fmt.Printf("%s", strings.TrimSuffix(color.CyanString(s), " "))
 }

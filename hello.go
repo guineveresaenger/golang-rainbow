ed030d8528586bf92625020339e6057fba554ee6
guineveresaenger <guineveresaenger@gmail.com>
2017-11-27 16:41:30 -0800

    this makes a rainbow now

--- hello.go
+++ hello.go
@@ -1,11 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fatih/color"
+)
 
 func main() {
-	fmt.Printf("Hello, Guinevere. You're a real dev now. No pressure...\n")
-	fmt.Printf("Hello, Guinevere. You're a real dev now. No pressure...\n")
-	fmt.Printf("Hello, Guinevere. You're a real dev now. No pressure...\n")
+	string := "Hello, Guinevere. You're a real dev now. No pressure...adding more chars to not run out of extra too much extra now not too much extra this can go on forrrrrevvvverrrrr"
 
+	for i := 0; i < 50; i++ {
+		rainbow(string, i)
+	}
 
+}
+
+func rainbow(s string, lineCount int) {
+	for i := 0; i < len(s); i++ {
+
+		subindex := (i + lineCount) % 30 // will give the index of each 30 char substring
+		// fmt.Printf("%d", subindex)
+		// subindex = (subindex + lineCount) % 30
+
+		switch {
+		case subindex >= 0 && subindex < 5:
+			red(string(s[i]))
+		case subindex >= 5 && subindex < 10:
+			yellow(string(s[i]))
+		case subindex >= 10 && subindex < 15:
+			green(string(s[i]))
+		case subindex >= 15 && subindex < 20:
+			blue(string(s[i]))
+		case subindex >= 20 && subindex < 25:
+			magenta(string(s[i]))
+		case subindex >= 25 && subindex < 30:
+			cyan(string(s[i]))
+		default:
+			fmt.Printf(string(s[i]))
+		}
+	}
+	fmt.Printf("\n")
+}
+
+func red(s string) {
+	fmt.Printf("%s", strings.TrimSuffix(color.RedString(s), " "))
+}
+
+func yellow(s string) {
+	fmt.Printf("%s", strings.TrimSuffix(color.YellowString(s), " "))
+}
+
+func blue(s string) {
+	fmt.Printf("%s", strings.TrimSuffix(color.BlueString(s), " "))
+}
+
+func green(s string) {
+	fmt.Printf("%s", strings.TrimSuffix(color.GreenString(s), " "))
+}
+
+func magenta(s string) {
+	fmt.Printf("%s", strings.TrimSuffix(color.MagentaString(s), " "))
+}
+
+func cyan(s string) {
+	fmt.Printf("%s", strings.TrimSuffix(color.CyanString(s), " "))
 }
